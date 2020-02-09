11f1a97b99f7f2081603023be97fdb5dcbf28e1f
Siddharth Prabhu <prabhulicious@gmail.com>
2020-02-09 17:04:02 +0000

    Query sensor and obtain data

--- sensor/read_sensor.py
+++ sensor/read_sensor.py
@@ -12,6 +12,9 @@ CMD_SET_SLEEP_AND_WORK = 6
 CMD_SET_WORKING_PERIOD = 8
 CMD_CHECK_FIRMWARE_VERSION = 7
 
+pm25 = []
+pm10 = []
+
 def gen_check_sum(data):
     sum = 0
     for d in data:
@@ -62,6 +65,38 @@ def cmd_set_sleep():
     send_command(gen_payload(CMD_SET_SLEEP_AND_WORK, data))
     read_response()
 
+def process_data(string):
+    lowpm = string[2]
+    highpm = string[4]
+    lowpm = int(lowpm)
+    highpm = int(highpm)
+    pm25.append(lowpm/10)
+    pm10.append(highpm/10)
+
+def cmd_query_data():
+    data = [0,0]
+    send_command(gen_payload(CMD_QUERY_DATA, data))
+    response = read_response()
+    process_data(response)
+
+def print_data():
+    total = 0
+    pm25.pop(0)
+    for i in pm25:
+        total = total + i
+    pm25_avg = round(total / len(pm25), 1)
+
+    total = 0
+    pm10.pop(0)
+    for i in pm10:
+        total = total + i
+    pm10_avg = round(total / len(pm10), 1)
+
+    print("PM2.5 > " + str(pm25_avg) + " PM10 > " + str(pm10_avg))
+
+
+
+
 ser = serial.Serial("/dev/ttyUSB0")
 
 ser.flushInput()
@@ -70,8 +105,13 @@ ser.baudrate = 9600
 ser.flushInput()
 
 cmd_set_work()
-time.sleep(5)
+
+for i in range(1, 7):
+  cmd_query_data()
+  time.sleep(1)
+  
 cmd_set_sleep()
 
 ser.close()
 
+print_data()
