2980913a6799789a8d2643fa2e08f7a64e1e00b5
Siddharth Prabhu <prabhulicious@gmail.com>
2020-02-09 15:36:07 +0000

    Add python file to read from sensor

---
+++ sensor/read_sensor.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3 -u
+
+import serial, time
+
+MODE_COMMAND = '\xb4'
+MODE_RESPONSE = '\xc5'
+
+CMD_DATA_REPORT_MODE = 2
+CMD_QUERY_DATA = 4
+CMD_SET_DEVICE_ID = 5
+CMD_SET_SLEEP_AND_WORK = 6
+CMD_SET_WORKING_PERIOD = 8
+CMD_CHECK_FIRMWARE_VERSION = 7
+
+def gen_payload(cmd, data):
+    payload = [chr(0)] * 19
+    payload[0] = '\xaa'
+    payload[1] = MODE_COMMAND
+    payload[2] = chr(cmd)
+    i = 3
+    for d in data:
+       payload[i] = chr(d)
+       i = i + 1
+    payload[15] = '\xff'
+    payload[16] = '\xff'
+    payload[17] = chr((sum(data) + cmd + ord(payload[15]) + ord(payload [16])) % 256)
+    payload[18] = '\xab'
+    return payload
+
+def send_command(payload):
+    payload_unicode = ''.join(payload)
+    ser.write(payload_unicode.encode('latin-1'))
+
+def cmd_set_work():
+    data = [
+            1, # Set Mode
+            1  # Work
+            ]
+    send_command(gen_payload(CMD_SET_SLEEP_AND_WORK, data))
+
+def cmd_set_sleep():
+    data = [
+            1, # Set mode
+            0  # Sleep
+           ]
+    send_command(gen_payload(CMD_SET_SLEEP_AND_WORK, data))
+
+
+ser = serial.Serial("/dev/ttyUSB0")
+
+ser.flushInput()
+print("Writing to " + ser.name)
+ser.baudrate = 9600
+ser.flushInput()
+
+cmd_set_work()
+time.sleep(5)
+cmd_set_sleep()
+
+ser.close()
+
