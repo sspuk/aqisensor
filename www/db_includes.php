bb0b57c1ec2a3d46d86a48414217d970e7a92328
Siddharth Prabhu <prabhulicious@gmail.com>
2020-03-01 18:45:43 +0000

    Fixed multiple bugs and added new checks

--- www/db_includes.php
+++ www/db_includes.php
@@ -8,8 +8,8 @@ $db_name = "pmeter";
 #checking validity of the values
 function verify_machine_values($identifier, $latitude, $longitude)
 {
-    $id_length = strlen($_POSTidentifier);
-    if ($id_length < 5 && $id_lenght > 99) {
+    $id_length = strlen($_POST["identifier"]);
+    if ($id_length < 5 && $id_length > 99) {
         print("<br>Error: Identifier must be in between 5 and 99 characters");
         return false;
     }
@@ -32,4 +32,30 @@ function verify_machine_values($identifier, $latitude, $longitude)
 
     return true;
 }
+
+function check_mc_id($mysql_conn, $mc_id)
+{
+    $sql = "SELECT * from machines where mc_id=$mc_id";
+    $result = mysqli_query($mysql_conn, $sql);
+    if (mysqli_num_rows($result) != 1) {
+        print("Invalid Machine id");
+        return false;
+    }
+
+    return true;
+}
+function check_record($pm25, $pm100)
+{
+    if (!(is_numeric($pm25) && is_numeric($pm100))) {
+        print("Invalid PM 2.5/10 values");
+        return false;
+    }
+    if ($pm25 < 0 || $pm25 > 1000 || $pm100 < 0 || $pm100 > 1000) {
+        print("Invalid PM 2.5/10 values");
+        return false;
+    }
+    return true;
+}
+
+
 ?>
